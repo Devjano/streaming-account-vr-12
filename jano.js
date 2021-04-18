d92b26792e2d2f593d5bbbf951e3c5fc4511542d
jano <77622056+Devjano@users.noreply.github.com>
2021-04-18 07:16:09 -0700

    Create jano.js

---
+++ jano.js
@@ -0,0 +1,23 @@
+
+const Discord = require("discord.js");
+const jano = new Discord.Client();
+//////// playing or dnd  streaming ////////
+jano.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `I rely on God that's why no one can destroy him`,
+  `BA XOM UP`
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    jano.user.setActivity(STREAMING, {      
+    type: "playing", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+//////////////// tokn lera da bne 
+jano.login("");
