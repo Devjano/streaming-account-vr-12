2d5af473fee75105515f856b9209c4abc3988e64
JANO <77622056+Devjano@users.noreply.github.com>
2021-11-03 01:57:24 -0700

    Update jano.js

--- jano.js
+++ jano.js
@@ -1,28 +1,21 @@
-
 const Discord = require("discord.js");
 const jano = new Discord.Client();
 //////// playing or dnd  streaming ////////
 jano.on("ready", () => {
-  console.log(
-    `Online In Servers`
-  );
-  let statuses = [
-  
-  `You only live once, so try to live for God.`,
- 
-  ];
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    jano.user.setActivity(STREAMING, {      
-    type: "STREAMING", 
-    url: "https://www.twitch.tv/faith"
+    jano.user.setActivity(STREAMING, {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 ///////////////////////
 jano.on("ready", () => {
-    var join = jano.channels.get(""); /// id voice
-    if (join) join.join();
-  });
-//////////////// tokn lera da bne 
+  var join = jano.channels.get(""); /// id voice
+  if (join) join.join();
+});
+//////////////// tokn lera da bne
 jano.login("");
